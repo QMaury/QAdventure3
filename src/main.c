a58244fb131af31b17dd483b4dd6641d108439cd
QMaury <octaviomaury1+1@gmail.com>
2022-02-08 14:07:45 -0800

    added a system(clear) to beginning

--- src/main.c
+++ src/main.c
@@ -24,6 +24,7 @@ int evilD = 0;
 
 int main() {
         x = y = ded = room = 0;
+        system("clear");
         printf("QAdventure3 0.0.1\nWritten by QMaury\n© 2021-2022");
         while (!ded || !evilD) {
                 if(x == 0 && y == -1)
