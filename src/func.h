37d637f14f58471c6e49a66ee9a80750e79d5969
QMaury <octaviomaury1+1@gmail.com>
2022-02-09 11:50:45 -0800

    swapped out © for "copyright" in header text

--- src/func.h
+++ src/func.h
@@ -1,5 +1,5 @@
 #define txtdiv printf("|=========================================|\n\n");
-#define head system("clear"); printf("QAdventure3 0.1.0\nWritten by QMaury\n© 2021-2022\n|=========================================|\n");  printRoomH();
+#define head system("clear"); printf("QAdventure3 0.1.0\nWritten by QMaury\nCopyright 2021-2022\n|=========================================|\n");  printRoomH();
 
 int getAction();
 void txtdvd();
