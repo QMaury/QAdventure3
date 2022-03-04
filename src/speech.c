4f58ca07e05ee584be394632b5f711e05185c7cc
QMaury <octaviomaury1+1@gmail.com>
2022-03-03 23:17:21 -0800

    added more speech options to first NPC, added colored text, moved some stuff around

--- src/speech.c
+++ src/speech.c
@@ -10,28 +10,41 @@ int have = 0;
 char playername[51] = "";
 
 int qGetPlayerName() {
-        char dude[5] = "dude";
-                                   
-        printf("What is your name?\n> ");
+        printf("\n\033[1;31m\"What is your name?\"\033[0m\n> ");
         fgets(playername, 50, stdin);
 	playername[strlen(playername)-1] = '\0';
-
-        int dudeB = strcasecmp(playername, dude);
-
-        if(dudeB == 0)
-                printf("\n%s. Now that's a name no one would self apply where I come from.\n", playername);
-        else
-                printf("\n%s, huh? Nice name.", playername);
-        cont();
 }
 
 int qtalk1() {
         if(have == 0) {
-                qGetPlayerName();
                 have = 1;
+                qGetPlayerName();
+                char dude[5] = "dude";
+                int dudeB = strcasecmp(playername, dude);
+
+                if(dudeB == 0)
+                        printf("\n\033[1;31m\"Dude. Now that's a name no one would self apply where I come from.\"\n", playername);
+                else
+                        printf("\n\033[1;31m\"%s, huh? Nice name.\n", playername);
+                printf(" My name is Walter.\"\033[0m\n");
+                cont();
         }
+
         else {
-                printf("Hello, %s.\n",playername);
+                printf("\n\033[1;31m\"Hello, %s.\n What do you want to talk about?\"\033[0m\n",playername);
+                printf("1) Ask about the area\n2) Ask why he's here\n3) Nevermind\n> ");
+                int resp = getAction();
+                printf("\n");
+                switch(resp) {
+                        case 1:
+                                printf("\033[1;31m\"lol this is a cave.\"\033[0m\n");
+                                break;
+                        case 2: 
+                                printf("\033[1;31mnothing here yet.\033[0m\n");
+                                break;
+                        case 3:
+                                break;
+                }
                 cont();
         }
 }
--- src/speech.h
+++ src/speech.h
@@ -1,5 +1,6 @@
 #define qtalk1() qtalk1() // in case i change name
 
 int qtalk(int npc);
+int qGetPlayerName();
 
 extern int npc;
